459624a1c2c2bb94816aae90bc6d3d4c63c8c344
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-22 13:49:23 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split("!").map {|str| str.split(".")}.flatten.map {|str| str.split("?")}.flatten.length
+    # this is ugly and I don't know how to shorten it
   end
 end
