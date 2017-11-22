49ed43a60d92b286c2ac764d9a853e44222e7d3c
nspp99b <chromepanthers@gmail.com>
2017-11-22 00:13:02 -0500

    solved using squeeze, count

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1] == "."? true : false
   end
 
   def question?
-
+    self[self.length-1] == "?"? true : false
   end
 
   def exclamation?
-
+    self[self.length-1] == "!"? true : false
   end
 
   def count_sentences
-
+    self.squeeze('.!?').count('.!?')
   end
 end
